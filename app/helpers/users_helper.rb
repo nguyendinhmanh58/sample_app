5f522775364fdca4fe136b8260a59948d842574e
Nguyen Dinh Manh <manhnd@kaopiz.com>
2016-10-08 18:59:28 +0700

    Add signup feature

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,13 @@
+module UsersHelper
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+  def full_title(title = null)
+    if title
+      return title;
+    end
+    return 'Static pages';
+  end
+end
